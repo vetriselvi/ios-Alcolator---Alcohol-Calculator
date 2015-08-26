737e8d5edbc1ddaf5347f4d382b3bf26898c00ff
Vetri <vairamut@usc.edu>
2015-08-26 06:04:26 -0700

    Assingment-20-Alcolator completed

---
+++ WhiskeyViewController.m
@@ -0,0 +1,54 @@
+//
+//  WhiskeyViewController.m
+//  alcolator
+//
+//  Created by Vetri Selvi Vairamuthu on 8/26/15.
+//  Copyright (c) 2015 Vetri Selvi Vairamuthu. All rights reserved.
+//
+
+#import "WhiskeyViewController.h"
+
+@interface WhiskeyViewController ()
+
+@end
+
+@implementation WhiskeyViewController
+
+- (void)buttonPressed:(UIButton *)sender;
+{
+    [self.beerPercentageTextField resignFirstResponder];
+    
+    int numberOfBeers = self.beerCountSlider.value;
+    int ouncesInOneBeerGlass = 12;  //assume they are 12oz beer bottles
+    
+    float alcoholPercentageOfBeer = [self.beerPercentageTextField.text floatValue] / 100;
+    float ouncesOfAlcoholPerBeer = ouncesInOneBeerGlass * alcoholPercentageOfBeer;
+    float ouncesOfAlcoholTotal = ouncesOfAlcoholPerBeer * numberOfBeers;
+    
+    float ouncesInOneWhiskeyGlass = 1;  // a 1oz shot
+    float alcoholPercentageOfWhiskey = 0.4;  // 40% is average
+    
+    float ouncesOfAlcoholPerWhiskeyGlass = ouncesInOneWhiskeyGlass * alcoholPercentageOfWhiskey;
+    float numberOfWhiskeyGlassesForEquivalentAlcoholAmount = ouncesOfAlcoholTotal / ouncesOfAlcoholPerWhiskeyGlass;
+    
+    NSString *beerText;
+    
+    if (numberOfBeers == 1) {
+        beerText = NSLocalizedString(@"beer", @"singular beer");
+    } else {
+        beerText = NSLocalizedString(@"beers", @"plural of beer");
+    }
+    
+    NSString *whiskeyText;
+    
+    if (numberOfWhiskeyGlassesForEquivalentAlcoholAmount == 1) {
+        whiskeyText = NSLocalizedString(@"shot", @"singular shot");
+    } else {
+        whiskeyText = NSLocalizedString(@"shots", @"plural of shot");
+    }
+    NSString *resultText = [NSString stringWithFormat:NSLocalizedString(@"%d %@ (with %.2f%% alcohol) contains as much alcohol as %.1f %@ of whiskey.", nil), numberOfBeers, beerText, [self.beerPercentageTextField.text floatValue], numberOfWhiskeyGlassesForEquivalentAlcoholAmount, whiskeyText];
+    self.resultLabel.text = resultText;
+}
+
+
+@end
